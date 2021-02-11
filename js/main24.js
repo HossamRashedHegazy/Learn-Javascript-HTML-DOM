c5fefd0fc03cd92e2b61fbb59abc5581d892c6bf
Hossam Rashad <bibo2010508@yahoo.com>
2021-02-11 02:57:12 +0200

    astert leason 24

--- js/main24.js
+++ js/main24.js
@@ -1,20 +1,22 @@
 /*global  document, window   */
 
 /*
- * Contains هو انو انت تداور على عنصار هال هو موجود فى العناصر داى ولال لا
+ * ClientHeight, CleintWidth
+ *
+ * viewable Area المكان اللى انت هتشوفو
+ *
+ * Include Padding بتحساب البادنج بتاع العنصر
+ *
+ * No Border مبتحسابش البوردار
+ *
+ * No Margin مبتحسابش المارجان
+ *
+ * no Scroll مبتحسابش الاسكراول
+ *
+ *
  * */
 
-/* ممكن تكتاب الفانكشان هنا و تستخدمه تحت من خلال اسمها بس  */
 
 var myMainDiv = document.getElementById('main');
 
-var myChild = document.getElementById('myChild');
-
-console.log(myMainDiv.contains(myChild)); // بشياك على الاداف الاصلى على عنصار جواه و لو تراو معناه انو العنصار موجود
-
-/* بشياك لو العنصار موجود غيارلى محتو الكلام اللى جواه الى حاجة تانى  */
-
-if (myMainDiv.contains(myChild)) {
-
-    myChild.textContent = 'none';
-}
+console.log(myMainDiv.clientHeight);
